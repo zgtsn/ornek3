fffcbc5af276a0eedf7cacdacb5dfeca7f9fac6d
ÖZGE <ozgetasin@gmail.com>
2020-11-24 22:33:21 +0300

    Sepete ekle kısmı yapıldı

--- src/test.java
+++ src/test.java
@@ -2,4 +2,9 @@ public class test {
     public static void main(String[] args) {
         System.out.println("1.işlem");
     }
+
+    public static  void yeniMethod(){
+        System.out.println("yeni methodun hazır");
+        System.out.println("sepete ekle kısımları buralar");
+    }
 }
